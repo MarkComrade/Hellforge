cc43c62cbc1341535992f701c341502648598715
BadoMark <badomark1019@gmail.com>
2026-01-16 19:53:39 +0100

    Home screen update

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -315,6 +315,9 @@ function Home() {
 
     //TODO: Home screen content
 
+    let gearLevel = 10; //placeholder values
+    let gold = 250;
+
     generateBootStrapGrid(1, 1, 12, 'homeMenuRow');
     let menuTitle = document.createElement('div');
     menuTitle.setAttribute('class', 'menuTitle homeMenu');
@@ -329,6 +332,70 @@ function Home() {
         'class',
         'characterInfoDiv col-sm-3 col-md-3 d-flex flex-column align-items-center'
     );
+
+    let userNameText = document.createElement('h2');
+    userNameText.innerHTML = 'User:' + userName;
+    userNameText.setAttribute('class', 'menuText');
+    homeUI[0].appendChild(userNameText);
+
+    let userImg = document.createElement('img');
+    userImg.setAttribute('src', '../textures/misc/defaultportrait.png');
+    userImg.setAttribute('class', 'characterImage img-fluid');
+    homeUI[0].appendChild(userImg);
+
+    //? Custom user image upload
+
+    let characterInfo = document.createElement('h3');
+    characterInfo.setAttribute('class', 'menuText');
+    characterInfo.innerHTML = 'Character Level:' + gearLevel;
+    homeUI[0].appendChild(characterInfo);
+
+    //! Character level = overall gear level
+
+    let goldAmount = document.createElement('h3');
+    goldAmount.setAttribute('class', 'menuText');
+    goldAmount.innerHTML = 'Gold:' + gold;
+    homeUI[0].appendChild(goldAmount);
+
+    let equipmentTitle = document.createElement('h2');
+    equipmentTitle.setAttribute('class', 'menuText');
+    equipmentTitle.innerHTML = 'Equipped gear:';
+    homeUI[0].appendChild(equipmentTitle);
+
+    let characterInfoDiv = document.querySelectorAll('.characterInfoDiv');
+
+    let armourRow = document.createElement('div');
+    armourRow.setAttribute('class', 'characterArmourRow');
+
+    let weaponsRow = document.createElement('div');
+    weaponsRow.setAttribute('class', 'characterWeaponsRow');
+
+    const equipmentSlots = [
+        { slot: 'Helmet', img: '../textures/items/armour/helmet_rusty.png', type: 'armour' },
+        { slot: 'Armor', img: '../textures/items/armour/armour_rusty.png', type: 'armour' },
+        { slot: 'Melee', img: '../textures/items/weapons/sword_rusty.png', type: 'weapon' },
+        { slot: 'Ranged', img: '../textures/items/weapons/bow_rusty.png', type: 'weapon' }
+    ];
+
+    equipmentSlots.forEach(({ slot, img, type }) => {
+        let slotDiv = document.createElement('div');
+        slotDiv.setAttribute(
+            'class',
+            'equipmentSlot col-sm-6 col-md-6 d-flex flex-column align-items-center'
+        );
+        let slotImg = document.createElement('img');
+        slotImg.setAttribute('src', img);
+        slotImg.setAttribute('class', 'itemImage img-fluid');
+        slotDiv.appendChild(slotImg);
+        if (type === 'armour') {
+            armourRow.appendChild(slotDiv);
+        } else {
+            weaponsRow.appendChild(slotDiv);
+        }
+    });
+
+    characterInfoDiv[0].appendChild(armourRow);
+    characterInfoDiv[0].appendChild(weaponsRow);
 
     homeUI[1].setAttribute(
         'class',
