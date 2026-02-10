4fad84d632355d9bee2f39d27acc0f429a93e78f
BadoMark <badomark1019@gmail.com>
2026-02-10 16:54:17 +0100

    In game inventory function
    WIP

--- frontend/javascript/menu.js
+++ frontend/javascript/menu.js
@@ -642,6 +642,10 @@ function checkOrientation() {
                 warning.style.backgroundColor = 'rgba(0, 0, 0, 1)';
                 body[0].appendChild(warning);
             }
+            let inventoryOverlay = document.querySelector('.inventoryOverlay');
+            if (inventoryOverlay) {
+                inventoryOverlay.remove();
+            }
             return false;
         } else {
             body[0].innerHTML = '';
@@ -747,3 +751,38 @@ function stopAudio() {
         audio.currentTime = 0;
     }
 }
+
+//Ingame menu handling
+
+function openInventory() {
+    let body = document.getElementsByTagName('body')[0];
+
+    const playerInventory = document.createElement('div');
+    playerInventory.setAttribute('class', 'playerInventory');
+
+    const inventoryOverlay = document.createElement('div');
+    inventoryOverlay.setAttribute('class', 'inventoryOverlay');
+
+    for (let i = 0; i < 2; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'inventoryRow');
+        inventoryOverlay.appendChild(row);
+        for (let j = 0; j < 5; j++) {
+            let cell = document.createElement('div');
+            cell.setAttribute('class', 'inventoryCell');
+            row.appendChild(cell);
+        }
+    }
+
+    let closeButton = document.createElement('input');
+    closeButton.setAttribute('type', 'button');
+    closeButton.setAttribute('value', 'Close Inventory');
+    closeButton.setAttribute('class', 'menuButton');
+    closeButton.addEventListener('click', () => {
+        inventoryOverlay.remove();
+    });
+
+    body.appendChild(inventoryOverlay);
+}
+
+function openSettings() {}
