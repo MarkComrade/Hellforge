81db5c1c9117d97039c57040513f978ef8f4a94e
BadoMark <badomark1019@gmail.com>
2026-02-12 19:47:11 +0100

    Update init.sql

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -1,1 +1,82 @@
 CREATE DATABASE IF NOT EXISTS hellforge_db;
+DEFAULT CHARACTER SET utf8
+COLLATE utf8_hungarian_ci;
+
+USE hellforge_db;
+
+CREATE TABLE `user`(
+    `userId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL
+);
+CREATE TABLE `player_inventory`(
+    `playerId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `gold` INT NOT NULL,
+    `helmet` INT NOT NULL,
+    `armor` INT NOT NULL,
+    `melee` INT NOT NULL,
+    `ranged` INT NOT NULL
+);
+CREATE TABLE `armors`(
+    `armorId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `img_path` VARCHAR(255) NOT NULL,
+    `tier` INT NOT NULL,
+    `price` INT NOT NULL,
+    `defense_multiplier` DOUBLE NOT NULL
+);
+CREATE TABLE `weapons`(
+    `weaponId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `type` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `img_path` VARCHAR(255) NOT NULL,
+    `tier` INT NOT NULL,
+    `price` INT NOT NULL,
+    `attack_multiplier` DOUBLE NOT NULL
+);
+
+INSERT INTO `armors` (`type`, `name`, `img_path`, `tier`, `price`, `defense_multiplier`) VALUES
+    ('Helmet', 'Rusty Helmet', '../textures/items/armour/helmet_rusty.png', 1, 5, 1),
+    ('Armor', 'Rusty Chestplate', '../textures/items/armour/armour_rusty.png', 1, 10, 1),
+    ('Helmet', 'Worn Helmet', '../textures/items/armour/helmet_worn.png', 2, 20, 1.1),
+    ('Armor', 'Worn Chestplate', '../textures/items/armour/armour_worn.png', 2, 50, 1.2),
+    ('Helmet', 'Regular Helmet', '../textures/items/armour/helmet_regular.png', 3, 60, 1.35),
+    ('Armor', 'Regular Chestplate', '../textures/items/armour/armour_regular.png', 3, 120, 1.55),
+    ('Helmet', 'Mythical Helmet', '../textures/items/armour/helmet_mythical.png', 4, 150, 1.65),
+    ('Armor', 'Mythical Chestplate', '../textures/items/armour/armour_mythical.png', 4, 300, 1.95),
+    ('Helmet', 'Legendary Helmet', '../textures/items/armour/helmet_legendary.png', 5, 350, 2.1),
+    ('Armor', 'Legendary Chestplate', '../textures/items/armour/armour_legendary.png', 5, 700, 2.5),
+    ('Helmet', 'Hellish Helmet', '../textures/items/armour/helmet_hellish.png', 6, 800, 2.8),
+    ('Armor', 'Hellish Chestplate', '../textures/items/armour/armour_hellish.png', 6, 1600, 3.4);
+
+INSERT INTO `weapons` (`type`, `name`, `img_path`, `tier`, `price`, `attack_multiplier`) VALUES
+    ('Melee', 'Rusty Sword', '../textures/items/weapons/sword_rusty.png', 1, 15, 1),
+    ('Ranged', 'Rusty Bow', '../textures/items/weapons/bow_rusty.png', 1, 10, 1),
+    ('Melee', 'Worn Sword', '../textures/items/weapons/sword_worn.png', 2, 60, 1.25),
+    ('Ranged', 'Worn Bow', '../textures/items/weapons/bow_worn.png', 2, 50, 1.15),
+    ('Melee', 'Regular Sword', '../textures/items/weapons/sword_regular.png', 3, 150, 1.65),
+    ('Ranged', 'Regular Bow', '../textures/items/weapons/bow_regular.png', 3, 120, 1.55),
+    ('Melee', 'Mythical Sword', '../textures/items/weapons/sword_mythical.png', 4, 380, 2.2),
+    ('Ranged', 'Mythical Bow', '../textures/items/weapons/bow_mythical.png', 4, 300, 2.0),
+    ('Melee', 'Legendary Sword', '../textures/items/weapons/sword_legendary.png', 5, 900, 2.8),
+    ('Ranged', 'Legendary Bow', '../textures/items/weapons/bow_legendary.png', 5, 700, 2.6),
+    ('Melee', 'Hellish Sword', '../textures/items/weapons/sword_hellish.png', 6, 2100, 3.6),
+    ('Ranged', 'Hellish Bow', '../textures/items/weapons/bow_hellish.png', 6, 1600, 3.2);
+
+CREATE TABLE `admin`(
+    `adminId` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` INT NOT NULL,
+    `password` INT NOT NULL
+);
+ALTER TABLE
+    `player_inventory` ADD CONSTRAINT `player_inventory_armor_foreign` FOREIGN KEY(`armor`) REFERENCES `armors`(`armorId`);
+ALTER TABLE
+    `player_inventory` ADD CONSTRAINT `player_inventory_ranged_foreign` FOREIGN KEY(`ranged`) REFERENCES `weapons`(`weaponId`);
+ALTER TABLE
+    `player_inventory` ADD CONSTRAINT `player_inventory_helmet_foreign` FOREIGN KEY(`helmet`) REFERENCES `armors`(`armorId`);
+ALTER TABLE
+    `player_inventory` ADD CONSTRAINT `player_inventory_playerid_foreign` FOREIGN KEY(`playerId`) REFERENCES `user`(`userId`);
+ALTER TABLE
+    `player_inventory` ADD CONSTRAINT `player_inventory_melee_foreign` FOREIGN KEY(`melee`) REFERENCES `weapons`(`weaponId`);
+
