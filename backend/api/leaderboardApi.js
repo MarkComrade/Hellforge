793030f578cb7d2b4bcf3c03c8944d47de6fd9d6
BadoMark <badomark1019@gmail.com>
2026-04-28 20:09:26 +0200

    Leaderboard queries

--- backend/api/leaderboardApi.js
+++ backend/api/leaderboardApi.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const database = require('../sql/queries/authUserQueries.js');
+const database = require('../sql/queries/leaderboardQueries.js');
 
 router.get('/', async (request, response) => {
     console.log('Leaderboard endpoint called');
